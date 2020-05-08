e2993f9f4dac756f5b716b8503159c2bdc091260
rajvishah13 <39721852+rajvishah13@users.noreply.github.com>
2020-05-08 16:54:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,4 +21,6 @@ mongoose.connect('mongodb://localhost:27017/battles',
 );
 
 //How do we listen to the port?
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
